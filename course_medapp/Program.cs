a9c381b7d58c78d19ec71ae01442b0543adf1afc
uneverdreamed <ma2005f@gmail.com>
2025-12-12 13:56:07 +0300

    добавлен абстрактный класс Person и методы GetFullName, GetAge и GetInfo

--- course_medapp/Program.cs
+++ course_medapp/Program.cs
@@ -6,18 +6,55 @@ using System.Windows.Forms;
 
 namespace course_medapp
 {
-    internal static class Program
+    public abstract class Person
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        protected Person()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Id = Guid.NewGuid().ToString();
+        }
+
+        protected Person(string firstName, string lastName, string phone, DateTime dob)
+        {
+            Id = Guid.NewGuid().ToString();
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phone;
+            DateOfBirth = dob;
+        }
+
+        public string GetFullName() => $"{LastName} {FirstName}";
+
+        public int GetAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+        public virtual string GetInfo()
+        {
+            return $"{GetFullName()}, {GetAge()} лет, тел: {PhoneNumber}";
+        }
+
+        internal static class Program
+        {
+            /// <summary>
+            ///  The main entry point for the application.
+            /// </summary>
+            [STAThread]
+            static void Main()
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
