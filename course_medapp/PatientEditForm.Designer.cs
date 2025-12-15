4f4a2835d85952e7867197fdc16a0b83d65214e3
uneverdreamed <ma2005f@gmail.com>
2025-12-15 16:26:29 +0300

    добавлена форма PatientEditForm

---
+++ course_medapp/PatientEditForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace course_medapp.Forms
+{
+    partial class PatientEditForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblInsurance = new System.Windows.Forms.Label();
+            this.txtInsurance = new System.Windows.Forms.TextBox();
+            this.lblMedCardNumber = new System.Windows.Forms.Label();
+            this.txtMedCardNumber = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(500, 60);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(182, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Новый пациент";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblFirstName.Location = new System.Drawing.Point(30, 80);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(40, 19);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "Имя:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtFirstName.Location = new System.Drawing.Point(170, 77);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(300, 25);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblLastName.Location = new System.Drawing.Point(30, 120);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(74, 19);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Фамилия:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtLastName.Location = new System.Drawing.Point(170, 117);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(300, 25);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblPhone.Location = new System.Drawing.Point(30, 160);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(69, 19);
+            this.lblPhone.TabIndex = 5;
+            this.lblPhone.Text = "Телефон:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtPhone.Location = new System.Drawing.Point(170, 157);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(300, 25);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblBirthDate.Location = new System.Drawing.Point(30, 200);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(110, 19);
+            this.lblBirthDate.TabIndex = 7;
+            this.lblBirthDate.Text = "Дата рождения:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.dtpBirthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthDate.Location = new System.Drawing.Point(170, 197);
+            this.dtpBirthDate.MaxDate = new System.DateTime(2025, 12, 31, 0, 0, 0, 0);
+            this.dtpBirthDate.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(300, 25);
+            this.dtpBirthDate.TabIndex = 8;
+            this.dtpBirthDate.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblAddress.Location = new System.Drawing.Point(30, 240);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(51, 19);
+            this.lblAddress.TabIndex = 9;
+            this.lblAddress.Text = "Адрес:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtAddress.Location = new System.Drawing.Point(170, 237);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(300, 60);
+            this.txtAddress.TabIndex = 10;
+            // 
+            // lblInsurance
+            // 
+            this.lblInsurance.AutoSize = true;
+            this.lblInsurance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblInsurance.Location = new System.Drawing.Point(30, 315);
+            this.lblInsurance.Name = "lblInsurance";
+            this.lblInsurance.Size = new System.Drawing.Size(131, 19);
+            this.lblInsurance.TabIndex = 11;
+            this.lblInsurance.Text = "Номер страховки:";
+            // 
+            // txtInsurance
+            // 
+            this.txtInsurance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtInsurance.Location = new System.Drawing.Point(170, 312);
+            this.txtInsurance.Name = "txtInsurance";
+            this.txtInsurance.Size = new System.Drawing.Size(300, 25);
+            this.txtInsurance.TabIndex = 12;
+            // 
+            // lblMedCardNumber
+            // 
+            this.lblMedCardNumber.AutoSize = true;
+            this.lblMedCardNumber.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblMedCardNumber.Location = new System.Drawing.Point(30, 355);
+            this.lblMedCardNumber.Name = "lblMedCardNumber";
+            this.lblMedCardNumber.Size = new System.Drawing.Size(119, 19);
+            this.lblMedCardNumber.TabIndex = 13;
+            this.lblMedCardNumber.Text = "Мед. карта №:";
+            // 
+            // txtMedCardNumber
+            // 
+            this.txtMedCardNumber.Enabled = false;
+            this.txtMedCardNumber.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtMedCardNumber.Location = new System.Drawing.Point(170, 352);
+            this.txtMedCardNumber.Name = "txtMedCardNumber";
+            this.txtMedCardNumber.ReadOnly = true;
+            this.txtMedCardNumber.Size = new System.Drawing.Size(300, 25);
+            this.txtMedCardNumber.TabIndex = 14;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(170, 400);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 40);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "💾 Сохранить";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(330, 400);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(140, 40);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // PatientEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 460);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtMedCardNumber);
+            this.Controls.Add(this.lblMedCardNumber);
+            this.Controls.Add(this.txtInsurance);
+            this.Controls.Add(this.lblInsurance);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PatientEditForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Пациент";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblInsurance;
+        private System.Windows.Forms.TextBox txtInsurance;
+        private System.Windows.Forms.Label lblMedCardNumber;
+        private System.Windows.Forms.TextBox txtMedCardNumber;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ course_medapp/PatientEditForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using course_medapp;
+using course_medapp.Services;
+
+namespace course_medapp.Forms
+{
+    public partial class PatientEditForm : Form
+    {
+        private DataManager dataManager;
+        private Patient patient;
+        private bool isEditMode;
+
+        public PatientEditForm()
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            isEditMode = false;
+            this.Text = "Добавить пациента";
+            lblTitle.Text = "Новый пациент";
+        }
+
+        public PatientEditForm(Patient existingPatient)
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            patient = existingPatient;
+            isEditMode = true;
+            this.Text = "Редактировать пациента";
+            lblTitle.Text = "Редактирование пациента";
+            LoadPatientData();
+        }
+
+        private void LoadPatientData()
+        {
+            txtFirstName.Text = patient.FirstName;
+            txtLastName.Text = patient.LastName;
+            txtPhone.Text = patient.PhoneNumber;
+            dtpBirthDate.Value = patient.DateOfBirth;
+            txtAddress.Text = patient.Address;
+            txtInsurance.Text = patient.InsuranceNumber;
+            txtMedCardNumber.Text = patient.MedicalCardNumber;
+            txtMedCardNumber.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+                return;
+
+            try
+            {
+                if (isEditMode)
+                {
+                    // Редактирование существующего пациента
+                    patient.FirstName = txtFirstName.Text.Trim();
+                    patient.LastName = txtLastName.Text.Trim();
+                    patient.PhoneNumber = txtPhone.Text.Trim();
+                    patient.DateOfBirth = dtpBirthDate.Value;
+                    patient.Address = txtAddress.Text.Trim();
+                    patient.InsuranceNumber = txtInsurance.Text.Trim();
+
+                    dataManager.UpdatePatient(patient);
+                    MessageBox.Show("Данные пациента обновлены", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Добавление нового пациента
+                    var newPatient = new Patient(
+                        txtFirstName.Text.Trim(),
+                        txtLastName.Text.Trim(),
+                        txtPhone.Text.Trim(),
+                        dtpBirthDate.Value,
+                        txtAddress.Text.Trim(),
+                        txtInsurance.Text.Trim()
+                    );
+
+                    dataManager.AddPatient(newPatient);
+                    MessageBox.Show("Пациент успешно добавлен", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Введите имя пациента", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtFirstName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Введите фамилию пациента", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLastName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Введите номер телефона", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPhone.Focus();
+                return false;
+            }
+
+            if (dtpBirthDate.Value >= DateTime.Now)
+            {
+                MessageBox.Show("Дата рождения не может быть в будущем", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
