0dcb65cead1171af9210767abd4c72848a5ba2d1
uneverdreamed <ma2005f@gmail.com>
2025-12-19 12:54:53 +0300

    добавлена форма DoctorEditForm

---
+++ course_medapp/DoctorEditForm.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace course_medapp.Forms
+{
+    partial class DoctorEditForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.lblSpecialization = new System.Windows.Forms.Label();
+            this.txtSpecialization = new System.Windows.Forms.TextBox();
+            this.lblLicense = new System.Windows.Forms.Label();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.lblExperience = new System.Windows.Forms.Label();
+            this.numExperience = new System.Windows.Forms.NumericUpDown();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.cmbDepartment = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numExperience)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(500, 60);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(140, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Новый врач";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblFirstName.Location = new System.Drawing.Point(30, 80);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(40, 19);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "Имя:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtFirstName.Location = new System.Drawing.Point(190, 77);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(280, 25);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblLastName.Location = new System.Drawing.Point(30, 115);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(74, 19);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Фамилия:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtLastName.Location = new System.Drawing.Point(190, 112);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(280, 25);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblPhone.Location = new System.Drawing.Point(30, 150);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(69, 19);
+            this.lblPhone.TabIndex = 5;
+            this.lblPhone.Text = "Телефон:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtPhone.Location = new System.Drawing.Point(190, 147);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(280, 25);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblBirthDate.Location = new System.Drawing.Point(30, 185);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(110, 19);
+            this.lblBirthDate.TabIndex = 7;
+            this.lblBirthDate.Text = "Дата рождения:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.dtpBirthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthDate.Location = new System.Drawing.Point(190, 182);
+            this.dtpBirthDate.MaxDate = new System.DateTime(2025, 12, 31, 0, 0, 0, 0);
+            this.dtpBirthDate.MinDate = new System.DateTime(1940, 1, 1, 0, 0, 0, 0);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(280, 25);
+            this.dtpBirthDate.TabIndex = 8;
+            this.dtpBirthDate.Value = new System.DateTime(1980, 1, 1, 0, 0, 0, 0);
+            // 
+            // lblSpecialization
+            // 
+            this.lblSpecialization.AutoSize = true;
+            this.lblSpecialization.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblSpecialization.Location = new System.Drawing.Point(30, 220);
+            this.lblSpecialization.Name = "lblSpecialization";
+            this.lblSpecialization.Size = new System.Drawing.Size(113, 19);
+            this.lblSpecialization.TabIndex = 9;
+            this.lblSpecialization.Text = "Специализация:";
+            // 
+            // txtSpecialization
+            // 
+            this.txtSpecialization.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtSpecialization.Location = new System.Drawing.Point(190, 217);
+            this.txtSpecialization.Name = "txtSpecialization";
+            this.txtSpecialization.Size = new System.Drawing.Size(280, 25);
+            this.txtSpecialization.TabIndex = 10;
+            // 
+            // lblLicense
+            // 
+            this.lblLicense.AutoSize = true;
+            this.lblLicense.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblLicense.Location = new System.Drawing.Point(30, 255);
+            this.lblLicense.Name = "lblLicense";
+            this.lblLicense.Size = new System.Drawing.Size(133, 19);
+            this.lblLicense.TabIndex = 11;
+            this.lblLicense.Text = "Номер лицензии:";
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtLicense.Location = new System.Drawing.Point(190, 252);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.Size = new System.Drawing.Size(280, 25);
+            this.txtLicense.TabIndex = 12;
+            // 
+            // lblExperience
+            // 
+            this.lblExperience.AutoSize = true;
+            this.lblExperience.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblExperience.Location = new System.Drawing.Point(30, 290);
+            this.lblExperience.Name = "lblExperience";
+            this.lblExperience.Size = new System.Drawing.Size(83, 19);
+            this.lblExperience.TabIndex = 13;
+            this.lblExperience.Text = "Стаж (лет):";
+            // 
+            // numExperience
+            // 
+            this.numExperience.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.numExperience.Location = new System.Drawing.Point(190, 287);
+            this.numExperience.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numExperience.Name = "numExperience";
+            this.numExperience.Size = new System.Drawing.Size(280, 25);
+            this.numExperience.TabIndex = 14;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblDepartment.Location = new System.Drawing.Point(30, 325);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(85, 19);
+            this.lblDepartment.TabIndex = 15;
+            this.lblDepartment.Text = "Отделение:";
+            // 
+            // cmbDepartment
+            // 
+            this.cmbDepartment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartment.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbDepartment.FormattingEnabled = true;
+            this.cmbDepartment.Location = new System.Drawing.Point(190, 322);
+            this.cmbDepartment.Name = "cmbDepartment";
+            this.cmbDepartment.Size = new System.Drawing.Size(280, 25);
+            this.cmbDepartment.TabIndex = 16;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(190, 370);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 40);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "💾 Сохранить";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(340, 370);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 40);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // DoctorEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 430);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cmbDepartment);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.numExperience);
+            this.Controls.Add(this.lblExperience);
+            this.Controls.Add(this.txtLicense);
+            this.Controls.Add(this.lblLicense);
+            this.Controls.Add(this.txtSpecialization);
+            this.Controls.Add(this.lblSpecialization);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DoctorEditForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Врач";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numExperience)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.Label lblSpecialization;
+        private System.Windows.Forms.TextBox txtSpecialization;
+        private System.Windows.Forms.Label lblLicense;
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Label lblExperience;
+        private System.Windows.Forms.NumericUpDown numExperience;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.ComboBox cmbDepartment;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ course_medapp/DoctorEditForm.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using course_medapp;
+using course_medapp.Services;
+
+namespace course_medapp.Forms
+{
+    public partial class DoctorEditForm : Form
+    {
+        private DataManager dataManager;
+        private Doctor doctor;
+        private bool isEditMode;
+
+        public DoctorEditForm()
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            isEditMode = false;
+            this.Text = "Добавить врача";
+            lblTitle.Text = "Новый врач";
+            LoadDepartments();
+        }
+
+        public DoctorEditForm(Doctor existingDoctor)
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            doctor = existingDoctor;
+            isEditMode = true;
+            this.Text = "Редактировать врача";
+            lblTitle.Text = "Редактирование врача";
+            LoadDepartments();
+            LoadDoctorData();
+        }
+
+        private void LoadDepartments()
+        {
+            cmbDepartment.Items.Clear();
+            cmbDepartment.Items.Add("-- Не выбрано --");
+
+            foreach (var dept in dataManager.Departments)
+            {
+                cmbDepartment.Items.Add(dept.Name);
+            }
+
+            cmbDepartment.SelectedIndex = 0;
+        }
+
+        private void LoadDoctorData()
+        {
+            txtFirstName.Text = doctor.FirstName;
+            txtLastName.Text = doctor.LastName;
+            txtPhone.Text = doctor.PhoneNumber;
+            dtpBirthDate.Value = doctor.DateOfBirth;
+            txtSpecialization.Text = doctor.Specialization;
+            txtLicense.Text = doctor.LicenseNumber;
+            numExperience.Value = doctor.ExperienceYears;
+
+            // Загружаем отделение
+            if (!string.IsNullOrEmpty(doctor.DepartmentId))
+            {
+                var department = dataManager.GetDepartmentById(doctor.DepartmentId);
+                if (department != null)
+                {
+                    int index = cmbDepartment.Items.IndexOf(department.Name);
+                    if (index >= 0)
+                        cmbDepartment.SelectedIndex = index;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+                return;
+
+            try
+            {
+                string departmentId = "";
+                if (cmbDepartment.SelectedIndex > 0)
+                {
+                    string deptName = cmbDepartment.SelectedItem.ToString();
+                    var dept = dataManager.Departments.FirstOrDefault(d => d.Name == deptName);
+                    if (dept != null)
+                        departmentId = dept.Id;
+                }
+
+                if (isEditMode)
+                {
+                    // Редактирование существующего врача
+                    doctor.FirstName = txtFirstName.Text.Trim();
+                    doctor.LastName = txtLastName.Text.Trim();
+                    doctor.PhoneNumber = txtPhone.Text.Trim();
+                    doctor.DateOfBirth = dtpBirthDate.Value;
+                    doctor.Specialization = txtSpecialization.Text.Trim();
+                    doctor.LicenseNumber = txtLicense.Text.Trim();
+                    doctor.ExperienceYears = (int)numExperience.Value;
+                    doctor.DepartmentId = departmentId;
+
+                    dataManager.UpdateDoctor(doctor);
+                    MessageBox.Show("Данные врача обновлены", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Добавление нового врача
+                    var newDoctor = new Doctor(
+                        txtFirstName.Text.Trim(),
+                        txtLastName.Text.Trim(),
+                        txtPhone.Text.Trim(),
+                        dtpBirthDate.Value,
+                        txtSpecialization.Text.Trim(),
+                        txtLicense.Text.Trim(),
+                        (int)numExperience.Value,
+                        departmentId
+                    );
+
+                    dataManager.AddDoctor(newDoctor);
+
+                    // Добавляем врача в отделение
+                    if (!string.IsNullOrEmpty(departmentId))
+                    {
+                        var dept = dataManager.GetDepartmentById(departmentId);
+                        if (dept != null)
+                        {
+                            dept.AddDoctor(newDoctor.Id);
+                            dataManager.UpdateDepartment(dept);
+                        }
+                    }
+
+                    MessageBox.Show("Врач успешно добавлен", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Введите имя врача", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtFirstName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Введите фамилию врача", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLastName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Введите номер телефона", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPhone.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtSpecialization.Text))
+            {
+                MessageBox.Show("Введите специализацию", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSpecialization.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLicense.Text))
+            {
+                MessageBox.Show("Введите номер лицензии", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLicense.Focus();
+                return false;
+            }
+
+            if (dtpBirthDate.Value >= DateTime.Now)
+            {
+                MessageBox.Show("Дата рождения не может быть в будущем", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (numExperience.Value < 0)
+            {
+                MessageBox.Show("Стаж не может быть отрицательным", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
