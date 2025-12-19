9044440b178e714e91ad7e914d1346be6953016f
uneverdreamed <ma2005f@gmail.com>
2025-12-19 13:36:35 +0300

    создана форма DepartmentForm

---
+++ course_medapp/DepartmentForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace course_medapp.Forms
+{
+    partial class DepartmentsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            panelTop = new System.Windows.Forms.Panel();
+            lblTitle = new System.Windows.Forms.Label();
+            splitContainer = new System.Windows.Forms.SplitContainer();
+            dgvDepartments = new System.Windows.Forms.DataGridView();
+            panelRight = new System.Windows.Forms.Panel();
+            lstDoctors = new System.Windows.Forms.ListBox();
+            lblDoctors = new System.Windows.Forms.Label();
+            panelBottom = new System.Windows.Forms.Panel();
+            btnClose = new System.Windows.Forms.Button();
+            lblTotal = new System.Windows.Forms.Label();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDepartments).BeginInit();
+            panelRight.SuspendLayout();
+            panelBottom.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = System.Drawing.Color.FromArgb(230, 126, 34);
+            panelTop.Controls.Add(lblTitle);
+            panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            panelTop.Location = new System.Drawing.Point(0, 0);
+            panelTop.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new System.Drawing.Size(1500, 115);
+            panelTop.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblTitle.ForeColor = System.Drawing.Color.White;
+            lblTitle.Location = new System.Drawing.Point(33, 29);
+            lblTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new System.Drawing.Size(209, 48);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Отделения";
+            // 
+            // splitContainer
+            // 
+            splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer.Location = new System.Drawing.Point(0, 115);
+            splitContainer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.Controls.Add(dgvDepartments);
+            // 
+            // splitContainer.Panel2
+            // 
+            splitContainer.Panel2.Controls.Add(panelRight);
+            splitContainer.Size = new System.Drawing.Size(1500, 808);
+            splitContainer.SplitterDistance = 916;
+            splitContainer.SplitterWidth = 7;
+            splitContainer.TabIndex = 1;
+            // 
+            // dgvDepartments
+            // 
+            dgvDepartments.AllowUserToAddRows = false;
+            dgvDepartments.AllowUserToDeleteRows = false;
+            dgvDepartments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDepartments.BackgroundColor = System.Drawing.Color.White;
+            dgvDepartments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDepartments.Dock = System.Windows.Forms.DockStyle.Fill;
+            dgvDepartments.Location = new System.Drawing.Point(0, 0);
+            dgvDepartments.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            dgvDepartments.MultiSelect = false;
+            dgvDepartments.Name = "dgvDepartments";
+            dgvDepartments.ReadOnly = true;
+            dgvDepartments.RowHeadersWidth = 62;
+            dgvDepartments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            dgvDepartments.Size = new System.Drawing.Size(916, 808);
+            dgvDepartments.TabIndex = 0;
+            dgvDepartments.CellContentClick += dgvDepartments_CellContentClick;
+            dgvDepartments.SelectionChanged += dgvDepartments_SelectionChanged;
+            // 
+            // panelRight
+            // 
+            panelRight.BackColor = System.Drawing.Color.White;
+            panelRight.Controls.Add(lstDoctors);
+            panelRight.Controls.Add(lblDoctors);
+            panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            panelRight.Location = new System.Drawing.Point(0, 0);
+            panelRight.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            panelRight.Name = "panelRight";
+            panelRight.Padding = new System.Windows.Forms.Padding(17, 19, 17, 19);
+            panelRight.Size = new System.Drawing.Size(577, 808);
+            panelRight.TabIndex = 0;
+            // 
+            // lstDoctors
+            // 
+            lstDoctors.Dock = System.Windows.Forms.DockStyle.Fill;
+            lstDoctors.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lstDoctors.FormattingEnabled = true;
+            lstDoctors.ItemHeight = 28;
+            lstDoctors.Location = new System.Drawing.Point(17, 67);
+            lstDoctors.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            lstDoctors.Name = "lstDoctors";
+            lstDoctors.Size = new System.Drawing.Size(543, 722);
+            lstDoctors.TabIndex = 1;
+            // 
+            // lblDoctors
+            // 
+            lblDoctors.Dock = System.Windows.Forms.DockStyle.Top;
+            lblDoctors.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblDoctors.Location = new System.Drawing.Point(17, 19);
+            lblDoctors.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            lblDoctors.Name = "lblDoctors";
+            lblDoctors.Size = new System.Drawing.Size(543, 48);
+            lblDoctors.TabIndex = 0;
+            lblDoctors.Text = "Врачи отделения:";
+            // 
+            // panelBottom
+            // 
+            panelBottom.BackColor = System.Drawing.Color.White;
+            panelBottom.Controls.Add(btnClose);
+            panelBottom.Controls.Add(lblTotal);
+            panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panelBottom.Location = new System.Drawing.Point(0, 923);
+            panelBottom.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            panelBottom.Name = "panelBottom";
+            panelBottom.Size = new System.Drawing.Size(1500, 115);
+            panelBottom.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = System.Drawing.Color.FromArgb(127, 140, 141);
+            btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnClose.ForeColor = System.Drawing.Color.White;
+            btnClose.Location = new System.Drawing.Point(1283, 19);
+            btnClose.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new System.Drawing.Size(167, 77);
+            btnClose.TabIndex = 1;
+            btnClose.Text = "Закрыть";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += btnClose_Click;
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblTotal.Location = new System.Drawing.Point(33, 38);
+            lblTotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new System.Drawing.Size(207, 30);
+            lblTotal.TabIndex = 0;
+            lblTotal.Text = "Всего отделений: 0";
+            // 
+            // DepartmentsForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1500, 1038);
+            Controls.Add(splitContainer);
+            Controls.Add(panelBottom);
+            Controls.Add(panelTop);
+            Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            Name = "DepartmentsForm";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Отделения";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvDepartments).EndInit();
+            panelRight.ResumeLayout(false);
+            panelBottom.ResumeLayout(false);
+            panelBottom.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.DataGridView dgvDepartments;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Label lblDoctors;
+        private System.Windows.Forms.ListBox lstDoctors;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ course_medapp/DepartmentForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using course_medapp;
+using course_medapp.Services;
+
+namespace course_medapp.Forms
+{
+    public partial class DepartmentsForm : Form
+    {
+        private DataManager dataManager;
+
+        public DepartmentsForm()
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            LoadDepartments();
+        }
+
+        private void LoadDepartments()
+        {
+            dgvDepartments.DataSource = null;
+            var departments = dataManager.Departments.Select(d => new
+            {
+                d.Id,
+                Название = d.Name,
+                Расположение = d.Location,
+                Телефон = d.PhoneNumber,
+                Описание = d.Description,
+                КоличествоВрачей = d.DoctorIds.Count
+            }).ToList();
+
+            dgvDepartments.DataSource = departments;
+            dgvDepartments.Columns["Id"].Visible = false;
+            lblTotal.Text = $"Всего отделений: {departments.Count}";
+        }
+
+        private void dgvDepartments_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvDepartments.SelectedRows.Count == 0)
+            {
+                lblDoctors.Text = "Врачи отделения:";
+                lstDoctors.Items.Clear();
+                return;
+            }
+
+            string departmentId = dgvDepartments.SelectedRows[0].Cells["Id"].Value.ToString();
+            string departmentName = dgvDepartments.SelectedRows[0].Cells["Название"].Value.ToString();
+
+            var department = dataManager.GetDepartmentById(departmentId);
+            if (department == null) return;
+
+            lblDoctors.Text = $"Врачи отделения \"{departmentName}\":";
+
+            lstDoctors.Items.Clear();
+            foreach (var doctorId in department.DoctorIds)
+            {
+                var doctor = dataManager.GetDoctorById(doctorId);
+                if (doctor != null)
+                {
+                    lstDoctors.Items.Add($"• {doctor.GetFullName()} - {doctor.Specialization}");
+                }
+            }
+
+            if (lstDoctors.Items.Count == 0)
+            {
+                lstDoctors.Items.Add("(Нет врачей в этом отделении)");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgvDepartments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
