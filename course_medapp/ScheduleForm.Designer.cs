39ae95b365d331a0decd589c301ffa303e21ccaf
uneverdreamed <ma2005f@gmail.com>
2025-12-19 13:05:34 +0300

    добавлена форма ScheduleForm

---
+++ course_medapp/ScheduleForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace course_medapp.Forms
+{
+    public partial class ScheduleForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.cmbDoctor = new System.Windows.Forms.ComboBox();
+            this.lblDoctor = new System.Windows.Forms.Label();
+            this.lblDoctorInfo = new System.Windows.Forms.Label();
+            this.dgvSchedule = new System.Windows.Forms.DataGridView();
+            this.lblNoAppointments = new System.Windows.Forms.Label();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            this.panelControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).BeginInit();
+            this.panelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(900, 60);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(240, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Расписание врачей";
+            // 
+            // panelControls
+            // 
+            this.panelControls.BackColor = System.Drawing.Color.White;
+            this.panelControls.Controls.Add(this.btnShow);
+            this.panelControls.Controls.Add(this.dtpDate);
+            this.panelControls.Controls.Add(this.lblDate);
+            this.panelControls.Controls.Add(this.cmbDoctor);
+            this.panelControls.Controls.Add(this.lblDoctor);
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControls.Location = new System.Drawing.Point(0, 60);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(900, 70);
+            this.panelControls.TabIndex = 1;
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShow.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnShow.ForeColor = System.Drawing.Color.White;
+            this.btnShow.Location = new System.Drawing.Point(750, 20);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(120, 30);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "📅 Показать";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(600, 23);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(130, 25);
+            this.dtpDate.TabIndex = 3;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblDate.Location = new System.Drawing.Point(540, 26);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(43, 19);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Дата:";
+            // 
+            // cmbDoctor
+            // 
+            this.cmbDoctor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDoctor.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbDoctor.FormattingEnabled = true;
+            this.cmbDoctor.Location = new System.Drawing.Point(80, 23);
+            this.cmbDoctor.Name = "cmbDoctor";
+            this.cmbDoctor.Size = new System.Drawing.Size(430, 25);
+            this.cmbDoctor.TabIndex = 1;
+            // 
+            // lblDoctor
+            // 
+            this.lblDoctor.AutoSize = true;
+            this.lblDoctor.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblDoctor.Location = new System.Drawing.Point(20, 26);
+            this.lblDoctor.Name = "lblDoctor";
+            this.lblDoctor.Size = new System.Drawing.Size(44, 19);
+            this.lblDoctor.TabIndex = 0;
+            this.lblDoctor.Text = "Врач:";
+            // 
+            // lblDoctorInfo
+            // 
+            this.lblDoctorInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.lblDoctorInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDoctorInfo.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDoctorInfo.Location = new System.Drawing.Point(0, 130);
+            this.lblDoctorInfo.Name = "lblDoctorInfo";
+            this.lblDoctorInfo.Padding = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.lblDoctorInfo.Size = new System.Drawing.Size(900, 40);
+            this.lblDoctorInfo.TabIndex = 2;
+            this.lblDoctorInfo.Text = "Выберите врача и дату";
+            // 
+            // dgvSchedule
+            // 
+            this.dgvSchedule.AllowUserToAddRows = false;
+            this.dgvSchedule.AllowUserToDeleteRows = false;
+            this.dgvSchedule.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSchedule.BackgroundColor = System.Drawing.Color.White;
+            this.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSchedule.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSchedule.Location = new System.Drawing.Point(0, 170);
+            this.dgvSchedule.Name = "dgvSchedule";
+            this.dgvSchedule.ReadOnly = true;
+            this.dgvSchedule.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSchedule.Size = new System.Drawing.Size(900, 310);
+            this.dgvSchedule.TabIndex = 3;
+            // 
+            // lblNoAppointments
+            // 
+            this.lblNoAppointments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblNoAppointments.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.lblNoAppointments.Location = new System.Drawing.Point(0, 170);
+            this.lblNoAppointments.Name = "lblNoAppointments";
+            this.lblNoAppointments.Size = new System.Drawing.Size(900, 310);
+            this.lblNoAppointments.TabIndex = 4;
+            this.lblNoAppointments.Text = "На выбранную дату нет записей";
+            this.lblNoAppointments.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNoAppointments.Visible = false;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.BackColor = System.Drawing.Color.White;
+            this.panelBottom.Controls.Add(this.btnClose);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 480);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(900, 60);
+            this.panelBottom.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(770, 10);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 40);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ScheduleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 540);
+            this.Controls.Add(this.lblNoAppointments);
+            this.Controls.Add(this.dgvSchedule);
+            this.Controls.Add(this.lblDoctorInfo);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelBottom);
+            this.Name = "ScheduleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расписание врачей";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelControls.ResumeLayout(false);
+            this.panelControls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).EndInit();
+            this.panelBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.Label lblDoctor;
+        private System.Windows.Forms.ComboBox cmbDoctor;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label lblDoctorInfo;
+        private System.Windows.Forms.DataGridView dgvSchedule;
+        private System.Windows.Forms.Label lblNoAppointments;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ course_medapp/ScheduleForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using course_medapp;
+using course_medapp.Services;
+using Microsoft.VisualBasic;
+
+namespace course_medapp.Forms
+{
+    public partial class ScheduleForm : Form
+    {
+        private DataManager dataManager;
+
+        public ScheduleForm()
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            LoadDoctors();
+            dtpDate.Value = DateTime.Today;
+        }
+
+        private void LoadDoctors()
+        {
+            cmbDoctor.DataSource = null;
+            cmbDoctor.DisplayMember = "DisplayText";
+            cmbDoctor.ValueMember = "Doctor";
+
+            var doctors = dataManager.Doctors.Select(d => new
+            {
+                Doctor = d,
+                DisplayText = $"{d.GetFullName()} - {d.Specialization}"
+            }).ToList();
+
+            cmbDoctor.DataSource = doctors;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (cmbDoctor.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите врача", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var doctor = (Doctor)cmbDoctor.SelectedValue;
+            var date = dtpDate.Value.Date;
+
+            lblDoctorInfo.Text = $"Врач: {doctor.GetFullName()} | Специализация: {doctor.Specialization} | Дата: {date:dd.MM.yyyy}";
+
+            var appointments = dataManager.GetDoctorAppointments(doctor.Id, date);
+
+            if (!appointments.Any())
+            {
+                lblNoAppointments.Visible = true;
+                dgvSchedule.Visible = false;
+            }
+            else
+            {
+                lblNoAppointments.Visible = false;
+                dgvSchedule.Visible = true;
+
+                var schedule = appointments.Select(a => new
+                {
+                    Время = a.AppointmentDateTime.ToString("HH:mm"),
+                    Длительность = $"{a.DurationMinutes} мин",
+                    Пациент = a.PatientName,
+                    Статус = GetStatusText(a.Status),
+                    Примечания = a.Notes
+                }).ToList();
+
+                dgvSchedule.DataSource = schedule;
+            }
+        }
+
+        private string GetStatusText(AppointmentStatus status)
+        {
+            return status switch
+            {
+                AppointmentStatus.Scheduled => "Запланирована",
+                AppointmentStatus.Completed => "Завершена",
+                AppointmentStatus.Cancelled => "Отменена",
+                _ => "Неизвестно"
+            };
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
