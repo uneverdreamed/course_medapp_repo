7e6a0a9313e6825e806a076f7daf48fcd0b04e09
uneverdreamed <ma2005f@gmail.com>
2025-12-19 12:59:16 +0300

    добавлена форма AppointmentsForm

---
+++ course_medapp/AppointmentsForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace course_medapp.Forms
+{
+    partial class AppointmentsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelFilter = new System.Windows.Forms.Panel();
+            this.cmbFilter = new System.Windows.Forms.ComboBox();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.dgvAppointments = new System.Windows.Forms.DataGridView();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.panelTop.SuspendLayout();
+            this.panelFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).BeginInit();
+            this.panelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(89)))), ((int)(((byte)(182)))));
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1000, 60);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(212, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Записи на приём";
+            // 
+            // panelFilter
+            // 
+            this.panelFilter.BackColor = System.Drawing.Color.White;
+            this.panelFilter.Controls.Add(this.cmbFilter);
+            this.panelFilter.Controls.Add(this.lblFilter);
+            this.panelFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFilter.Location = new System.Drawing.Point(0, 60);
+            this.panelFilter.Name = "panelFilter";
+            this.panelFilter.Size = new System.Drawing.Size(1000, 60);
+            this.panelFilter.TabIndex = 1;
+            // 
+            // cmbFilter
+            // 
+            this.cmbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilter.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.cmbFilter.FormattingEnabled = true;
+            this.cmbFilter.Items.AddRange(new object[] {
+            "Все записи",
+            "Запланированные",
+            "Завершённые",
+            "Отменённые",
+            "Сегодня"});
+            this.cmbFilter.Location = new System.Drawing.Point(100, 17);
+            this.cmbFilter.Name = "cmbFilter";
+            this.cmbFilter.Size = new System.Drawing.Size(250, 28);
+            this.cmbFilter.TabIndex = 1;
+            this.cmbFilter.SelectedIndexChanged += new System.EventHandler(this.cmbFilter_SelectedIndexChanged);
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblFilter.Location = new System.Drawing.Point(20, 20);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(63, 20);
+            this.lblFilter.TabIndex = 0;
+            this.lblFilter.Text = "Фильтр:";
+            // 
+            // dgvAppointments
+            // 
+            this.dgvAppointments.AllowUserToAddRows = false;
+            this.dgvAppointments.AllowUserToDeleteRows = false;
+            this.dgvAppointments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAppointments.BackgroundColor = System.Drawing.Color.White;
+            this.dgvAppointments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAppointments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAppointments.Location = new System.Drawing.Point(0, 120);
+            this.dgvAppointments.MultiSelect = false;
+            this.dgvAppointments.Name = "dgvAppointments";
+            this.dgvAppointments.ReadOnly = true;
+            this.dgvAppointments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAppointments.Size = new System.Drawing.Size(1000, 380);
+            this.dgvAppointments.TabIndex = 2;
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.BackColor = System.Drawing.Color.White;
+            this.panelButtons.Controls.Add(this.btnClose);
+            this.panelButtons.Controls.Add(this.btnComplete);
+            this.panelButtons.Controls.Add(this.btnCancel);
+            this.panelButtons.Controls.Add(this.btnAdd);
+            this.panelButtons.Controls.Add(this.lblTotal);
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelButtons.Location = new System.Drawing.Point(0, 500);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(1000, 70);
+            this.panelButtons.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(870, 15);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(110, 40);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnComplete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComplete.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnComplete.ForeColor = System.Drawing.Color.White;
+            this.btnComplete.Location = new System.Drawing.Point(290, 15);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(130, 40);
+            this.btnComplete.TabIndex = 3;
+            this.btnComplete.Text = "✔️ Завершить";
+            this.btnComplete.UseVisualStyleBackColor = false;
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(140, 15);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 40);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "❌ Отменить";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(20, 15);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 40);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "➕ Создать";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblTotal.Location = new System.Drawing.Point(480, 25);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(117, 20);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Text = "Всего записей: 0";
+            // 
+            // AppointmentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 570);
+            this.Controls.Add(this.dgvAppointments);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.panelFilter);
+            this.Controls.Add(this.panelTop);
+            this.Name = "AppointmentsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Записи на приём";
+            this.Load += new System.EventHandler(this.AppointmentsForm_Load);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelFilter.ResumeLayout(false);
+            this.panelFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).EndInit();
+            this.panelButtons.ResumeLayout(false);
+            this.panelButtons.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private void AppointmentsForm_Load(object sender, System.EventArgs e)
+        {
+            cmbFilter.SelectedIndex = 0;
+        }
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelFilter;
+        private System.Windows.Forms.ComboBox cmbFilter;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.DataGridView dgvAppointments;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
---
+++ course_medapp/AppointmentsForm.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using course_medapp;
+using course_medapp.Services;
+
+namespace course_medapp.Forms
+{
+    public partial class AppointmentsForm : Form
+    {
+        private DataManager dataManager;
+
+        public AppointmentsForm()
+        {
+            InitializeComponent();
+            dataManager = DataManager.Instance;
+            LoadAppointments();
+        }
+
+        private void LoadAppointments()
+        {
+            dgvAppointments.DataSource = null;
+            var appointments = dataManager.Appointments
+                .OrderByDescending(a => a.AppointmentDateTime)
+                .Select(a => new
+                {
+                    a.Id,
+                    Дата = a.AppointmentDateTime.ToString("dd.MM.yyyy"),
+                    Время = a.AppointmentDateTime.ToString("HH:mm"),
+                    Пациент = a.PatientName,
+                    Врач = a.DoctorName,
+                    Статус = GetStatusText(a.Status),
+                    Примечания = a.Notes
+                }).ToList();
+
+            dgvAppointments.DataSource = appointments;
+            dgvAppointments.Columns["Id"].Visible = false;
+            lblTotal.Text = $"Всего записей: {appointments.Count}";
+        }
+
+        private string GetStatusText(AppointmentStatus status)
+        {
+            return status switch
+            {
+                AppointmentStatus.Scheduled => "Запланирована",
+                AppointmentStatus.Completed => "Завершена",
+                AppointmentStatus.Cancelled => "Отменена",
+                AppointmentStatus.NoShow => "Не пришёл",
+                _ => "Неизвестно"
+            };
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AppointmentEditForm editForm = new AppointmentEditForm();
+            if (editForm.ShowDialog() == DialogResult.OK)
+            {
+                LoadAppointments();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (dgvAppointments.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Выберите запись для отмены", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string appointmentId = dgvAppointments.SelectedRows[0].Cells["Id"].Value.ToString();
+            var appointment = dataManager.Appointments.FirstOrDefault(a => a.Id == appointmentId);
+
+            if (appointment == null)
+            {
+                MessageBox.Show("Запись не найдена", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!appointment.CanBeCancelled())
+            {
+                MessageBox.Show("Эту запись нельзя отменить.\nОтменить можно только запланированные записи за 2+ часа до приёма.",
+                    "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var result = MessageBox.Show(
+                $"Вы уверены, что хотите отменить запись?\n\n" +
+                $"Пациент: {appointment.PatientName}\n" +
+                $"Врач: {appointment.DoctorName}\n" +
+                $"Дата: {appointment.AppointmentDateTime:dd.MM.yyyy HH:mm}",
+                "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    dataManager.CancelAppointment(appointmentId);
+                    LoadAppointments();
+                    MessageBox.Show("Запись отменена", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка отмены: {ex.Message}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            if (dgvAppointments.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Выберите запись для завершения", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string appointmentId = dgvAppointments.SelectedRows[0].Cells["Id"].Value.ToString();
+
+            var result = MessageBox.Show("Отметить запись как завершённую?",
+                "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    dataManager.CompleteAppointment(appointmentId);
+                    LoadAppointments();
+                    MessageBox.Show("Запись завершена", "Успех",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void cmbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbFilter.SelectedIndex == 0) // Все
+            {
+                LoadAppointments();
+                return;
+            }
+
+            var allAppointments = dataManager.Appointments.OrderByDescending(a => a.AppointmentDateTime);
+
+            var filtered = cmbFilter.SelectedIndex switch
+            {
+                1 => allAppointments.Where(a => a.Status == AppointmentStatus.Scheduled), // Запланированные
+                2 => allAppointments.Where(a => a.Status == AppointmentStatus.Completed), // Завершённые
+                3 => allAppointments.Where(a => a.Status == AppointmentStatus.Cancelled), // Отменённые
+                4 => allAppointments.Where(a => a.AppointmentDateTime.Date == DateTime.Today), // Сегодня
+                _ => allAppointments
+            };
+
+            var result = filtered.Select(a => new
+            {
+                a.Id,
+                Дата = a.AppointmentDateTime.ToString("dd.MM.yyyy"),
+                Время = a.AppointmentDateTime.ToString("HH:mm"),
+                Пациент = a.PatientName,
+                Врач = a.DoctorName,
+                Статус = GetStatusText(a.Status),
+                Примечания = a.Notes
+            }).ToList();
+
+            dgvAppointments.DataSource = result;
+            dgvAppointments.Columns["Id"].Visible = false;
+            lblTotal.Text = $"Найдено: {result.Count}";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
